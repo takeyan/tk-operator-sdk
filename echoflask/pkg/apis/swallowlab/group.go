e7185ef5bf9b273cde5d82a48208dff2abc0d440
Seiichi Takeuchi <takeyan.g@gmail.com>
2020-06-20 11:34:20 +0000

    verification completed successfully

---
+++ echoflask/pkg/apis/swallowlab/group.go
@@ -0,0 +1,6 @@
+// Package swallowlab contains swallowlab API versions.
+//
+// This file ensures Go source parsers acknowledge the swallowlab package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package swallowlab
---
+++ echoflask/pkg/apis/swallowlab/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the swallowlab v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=swallowlab.work
+package v1alpha1
